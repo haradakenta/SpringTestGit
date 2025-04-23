4815f357bd2609546667bc28850febb0655b0e1d
kenta <kenta@192.168.1.13>
2025-04-23 15:58:11 +0900

    更新

--- SpringBootProj/src/main/java/com/example/demo/service/InquiryService.java
+++ SpringBootProj/src/main/java/com/example/demo/service/InquiryService.java
@@ -13,9 +13,18 @@ public class InquiryService {
  *@param dataForm
  *@return 
  */
- public Boolean doInquiryEntry(InquiryForm dataForm)
+ public Boolean doInquiryEntry(InquiryFormViewModel dataForm)
  {
  
+//追加コード
+//受付日時の生成・設定
+DataTimeFormatter dtf = DataTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+dataForm.setRecdt(dtf.format(LocalDatetime.now()));
+
+//受付番号の生成・設定
+DataTimeFormatter dtNo = DataTimeFormatter.ofPattern("yyyyMMddHHmmss");
+dataForm.setRecno("NO" + dtNo.format(LocalDatetime.now()));
+
 	 
  //登録チェック
  if(!checkInquiry(dataForm))
